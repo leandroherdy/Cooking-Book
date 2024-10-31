e448d25fc48ead706613ec3390e1effb807a61cd
Leandro Vieira <le.herdy@gmail.com>
2024-10-31 18:59:45 -0300

    chore: add Dockerfile, docker-compose configurations, and Makefile

--- Makefile
+++ Makefile
@@ -26,5 +26,10 @@ run-server:
 superuser:
 	poetry run python -m core.manage createsuperuser
 
+.PHONY: up-dependencies-only
+up-dependencies-only:
+	test -f .env || touch .env
+	docker-compose -f docker-compose.dev.yml up --force-recreate db
+
 .PHONY: update
 update: install migrate install-pre-commit ;
--- core/project/settings.py
+++ core/project/settings.py
@@ -74,8 +74,14 @@ WSGI_APPLICATION = 'core.project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cooking_book',
+        'USER': 'cooking_book',
+        'PASSWORD': 'cooking_book',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'ATOMIC_REQUESTS': True,
+        'CONN_MAX_AGE': 0,
     }
 }
 
